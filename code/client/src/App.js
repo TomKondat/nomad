18158acb800fd0c1e37e070d8021f53e8b82746c
Eden Yemini <eden881@gmail.com>
2023-03-21 01:13:47 +0200

    Implement a React Router

--- code/client/src/App.js
+++ code/client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return (<React.Fragment></React.Fragment>);
+  return (<React.Fragment>
+    <div><h1>Hello world!</h1></div>
+  </React.Fragment>);
 }
 
 export default App;
--- code/client/src/index.js
+++ code/client/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
