e42a7ac315e500dfbc738f8ba278611d00a67d80
Eden Yemini <eden881@gmail.com>
2023-03-21 05:02:33 +0200

    Create UserProfile model

--- code/server/backend/backend/settings.py
+++ code/server/backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- code/server/backend/profiles/models.py
+++ code/server/backend/profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthdate = models.DateField()
+    address = models.CharField(max_length=64)
+    lat = models.DecimalField('last known latitude', max_digits=9, decimal_places=6)
+    lon = models.DecimalField('last known longitude', max_digits=9, decimal_places=6)
+
+    # TODO: Find a way to add profile picture to this model
