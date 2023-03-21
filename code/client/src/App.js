58b365b9c97da9b8d312eebc280504f2f13deb70
Eden Yemini <eden881@gmail.com>
2023-03-21 02:58:03 +0200

    Implement client side navigation with React Router

--- code/client/src/App.js
+++ code/client/src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Header from "./Header";
+
+// Routes
+import Home from "./routes/Home";
+import Friends from "./routes/Friends";
+import Profile from "./routes/Profile";
 
 function App() {
   return (
     <React.Fragment>
-      <Header></Header>
+      <BrowserRouter>
+        <Header></Header>
+        <Container className="pt-2">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="friends" element={<Friends />} />
+            <Route path="profile" element={<Profile />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
--- code/client/src/index.js
+++ code/client/src/index.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
