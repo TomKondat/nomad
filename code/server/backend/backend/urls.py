4af937d14d79e0739128f70a8c295bd4e30bfd59
Tom Kondat <tomkondat5@gmail.com>
2023-03-22 13:32:42 +0200

    create basic django crud

--- code/server/backend/backend/urls.py
+++ code/server/backend/backend/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'firstname', 'lastname']
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
