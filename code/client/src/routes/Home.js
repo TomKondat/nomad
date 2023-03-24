8e7de3b2faca6ae429aac594e29604bc0135a80b
Tom Kondat <tomkondat5@gmail.com>
2023-03-24 22:10:42 +0300

    adding "fake" conference to json

--- code/client/src/routes/Home.js
+++ code/client/src/routes/Home.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Home() {
-  return (
-    <React.Fragment>
-      <h1>Home page</h1>
-    </React.Fragment>
-  );
+  return <React.Fragment></React.Fragment>;
 }
 
 export default Home;
