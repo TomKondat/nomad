ae1c01a63ed6af1aecf4e5603be8e69cb74c9680
Tom Kondat <tomkondat5@gmail.com>
2023-03-25 00:58:42 +0300

    adding fields to cList & design basic card

--- code/client/src/routes/Home.js
+++ code/client/src/routes/Home.js
@@ -1,7 +1,45 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import conventions from "../conventionList";
+import ListGroup from "react-bootstrap/ListGroup";
+import { FcOvertime } from "react-icons/fc";
+import { FaSearchLocation } from "react-icons/fa";
+import { GiNightSleep } from "react-icons/gi";
+import { GiPathDistance } from "react-icons/gi";
 
 function Home() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      {conventions.map((conv) => (
+        <Card key={conv.id} style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={conv.img} />
+          <Card.Body>
+            <Card.Title>{conv.title}</Card.Title>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>
+                <GiPathDistance />
+                {conv.km} KM away from me!
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <FaSearchLocation />
+                {conv.city}, {conv.location}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <FcOvertime />
+                {conv.starting_time}{" "}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <GiNightSleep />
+                {conv.ending_time}
+              </ListGroup.Item>
+            </ListGroup>
+            <Button variant="outline-dark">More details</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </React.Fragment>
+  );
 }
 
 export default Home;
