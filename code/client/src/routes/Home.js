f74053482ef9e4404e266cb47157c4425f7725d4
Eden Yemini <eden881@gmail.com>
2023-03-27 01:29:15 +0300

    Integrate search bar to home page

--- code/client/src/routes/Home.js
+++ code/client/src/routes/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 import conventions from "../conventionList";
-import SizesExample from "./searchBar";
 
 import {
+  BsSearch,
   BsPinMapFill,
   BsClockFill,
   BsClockHistory,
@@ -16,8 +18,17 @@ import {
 function Home() {
   return (
     <React.Fragment>
-      <br />
-      <SizesExample></SizesExample>
+      <InputGroup size="sm" className="mb-3 mt-1">
+        <InputGroup.Text id="inputGroup-sizing-sm">
+          {" "}
+          <BsSearch />
+        </InputGroup.Text>
+        <Form.Control
+          placeholder="Search Convention"
+          aria-label="Small"
+          aria-describedby="inputGroup-sizing-sm"
+        />
+      </InputGroup>
       {conventions.map((conv) => (
         <Card key={conv.id} className=" mt-3" style={{ width: "22rem" }}>
           <Card.Img variant="top" src={conv.img} />
