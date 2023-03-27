a34fc4d7019caaad826e22e72688c458f4a5ab2a
Eden Yemini <eden881@gmail.com>
2023-03-27 03:01:46 +0300

    Redesign cards

--- code/client/src/routes/Home.js
+++ code/client/src/routes/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+
 import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
 import conventions from "../conventionList";
+import "./Home.css";
 
 import {
   BsSearch,
@@ -23,39 +26,47 @@ function Home() {
           {" "}
           <BsSearch />
         </InputGroup.Text>
-        <Form.Control
-          placeholder="Search Convention"
-          aria-label="Small"
-          aria-describedby="inputGroup-sizing-sm"
-        />
+        <Form.Control placeholder="Search Convention" />
       </InputGroup>
-      {conventions.map((conv) => (
-        <Card key={conv.id} className=" mt-3" style={{ width: "22rem" }}>
-          <Card.Img variant="top" src={conv.img} />
-          <Card.Body>
-            <Card.Title>{conv.title}</Card.Title>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>
-                <BsSignTurnRightFill />
-                &nbsp;{conv.km}km away from you
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <BsPinMapFill />
-                &nbsp;{conv.city}, {conv.location}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <BsClockFill />
-                &nbsp;{conv.starting_time}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <BsClockHistory />
-                &nbsp;{conv.ending_time}
-              </ListGroup.Item>
-            </ListGroup>
-            <Button variant="outline-dark">More details</Button>
-          </Card.Body>
-        </Card>
-      ))}
+      <Row
+        xs={1}
+        sm={2}
+        md={3}
+        lg={4}
+        className="g-4 d-flex justify-content-center"
+      >
+        {conventions.map((conv) => (
+          <Col key={conv.id} className="d-flex align-items-stretch">
+            <Card
+              className="p-0 pb-0 shadow convcard"
+              style={{ width: "22rem" }}
+            >
+              <Card.Img variant="top" src={conv.img} className="convcardimg" />
+              <Card.Body>
+                <Card.Title>{conv.title}</Card.Title>
+              </Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <BsSignTurnRightFill />
+                  &nbsp;{conv.km}km away from you
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <BsPinMapFill />
+                  &nbsp;{conv.city}, {conv.location}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <BsClockFill />
+                  &nbsp;{conv.starting_time}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <BsClockHistory />
+                  &nbsp;{conv.ending_time}
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </React.Fragment>
   );
 }
