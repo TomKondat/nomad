e6d8bcbef0ab8eb71a328e4fa27b6a38f3215d15
Eden Yemini <eden881@gmail.com>
2023-04-10 16:37:27 +0300

    Include profiles app urls

--- code/server/backend/backend/urls.py
+++ code/server/backend/backend/urls.py
@@ -15,14 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from . import rest_framework
-
-# restframework router
-router = routers.DefaultRouter()
-router.register(r'users', rest_framework.UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/', include('profiles.urls')),
 ]
