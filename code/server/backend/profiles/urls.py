8de83c7da74645ef66240e1e8298bead02b7930d
Eden Yemini <eden881@gmail.com>
2023-04-10 21:10:30 +0300

    Activate JWT

--- code/server/backend/profiles/urls.py
+++ code/server/backend/profiles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import SignUpView, LoginView
 
 urlpatterns = [
-    path('signup', views.SignUpView.as_view()),
-    path('login', views.LoginView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup/', SignUpView.as_view()),
+    path('login/', LoginView.as_view()),
 ]
