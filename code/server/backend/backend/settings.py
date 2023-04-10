026d4066addd7be20ccff163de69321d916c154d
Eden Yemini <eden881@gmail.com>
2023-04-10 19:19:55 +0300

    Tweak settings and migrations

--- code/server/backend/backend/settings.py
+++ code/server/backend/backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -128,11 +129,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
--- code/server/backend/profiles/models.py
+++ code/server/backend/profiles/models.py
@@ -6,7 +6,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthdate = models.DateField()
     address = models.CharField(max_length=64)
-    lat = models.DecimalField('last known latitude', max_digits=9, decimal_places=6)
-    lon = models.DecimalField('last known longitude', max_digits=9, decimal_places=6)
+    lat = models.DecimalField('last known latitude', blank=True, null=True, max_digits=9, decimal_places=6)
+    lon = models.DecimalField('last known longitude', blank=True, null=True, max_digits=9, decimal_places=6)
 
     # TODO: Find a way to add profile picture to this model
