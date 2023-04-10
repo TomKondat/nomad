8ce5ecac18a76a04b18c53950fb0455155fea5b3
Eden Yemini <eden881@gmail.com>
2023-04-10 21:19:38 +0300

    Create JWT URLs

--- code/server/backend/backend/settings.py
+++ code/server/backend/backend/settings.py
@@ -132,6 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
--- code/server/backend/profiles/urls.py
+++ code/server/backend/profiles/urls.py
@@ -3,8 +3,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import SignUpView, LoginView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('signup/', SignUpView.as_view()),
-    path('login/', LoginView.as_view()),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup', SignUpView.as_view()),
+    path('login', LoginView.as_view()),
 ]
