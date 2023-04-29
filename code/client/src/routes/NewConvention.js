a392a998ac9f1597f801cd7732cbd0815c9a0824
Tom Kondat <tomkondat5@gmail.com>
2023-04-29 22:19:06 +0300

    GET request from backend and Axios from client

--- code/client/src/routes/NewConvention.js
+++ code/client/src/routes/NewConvention.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import {
   Container,
   Row,
@@ -22,126 +23,147 @@ export default function NewConvention() {
   const handleClick = () => {
     navigate("/EditConvention");
   };
-
+  const [convention, setConvention] = useState();
   let params = useParams();
+
+  async function getConvention() {
+    await axios
+      .get(`http://127.0.0.1:8000/api/get-convention/?q=${params.conventionId}`)
+      .then((res) => {
+        setConvention(res.data);
+      })
+      .catch((error) => console.log(error));
+  }
+  useEffect(() => {
+    getConvention();
+  }, [params]);
 
   return (
     <Container>
-      <Row>
-        <Col>
-          <Button
-            onClick={() => {
-              handleClick();
-            }}
-            className="rounded-pill"
-            variant="light"
-          >
-            <FiEdit />
-          </Button>
-          <h1 className="display-4 text-center my-4">
-            Convention Number {params.conventionId}
-          </h1>
-          <div
-            style={{
-              position: "relative",
-              overflow: "hidden",
-              borderRadius: "10px",
-            }}
-          >
-            <Image
-              src="https://vrscout.com/wp-content/uploads/2020/03/GoogleIO2020_2-copy.png"
-              alt="Convention Image"
-              fluid
-              style={{ filter: "brightness(80%)" }}
-            />
-            <div
-              style={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                color: "#fff",
-                zIndex: 1,
-              }}
-            >
-              <h2 style={{ fontSize: "2rem" }}>Convention</h2>
-            </div>
-          </div>
-          <div className="my-4">
-            <p className="lead">short description here</p>
-          </div>
-        </Col>
-        <Col lg={4} className="my-4">
-          <div className="card border-0 shadow-sm rounded">
-            <div className="card-body">
-              <h3 className="card-title mb-3">Details</h3>
-              <ul className="list-unstyled">
-                <li>
-                  <strong>Address:</strong> Harama 52, Dimona
-                </li>
-                <li>
-                  <strong>Capacity:</strong> 500
-                </li>
-                <li>
-                  <strong>Start Time:</strong> 9:00 AM
-                </li>
-                <li>
-                  <strong>End Time:</strong> 5:00 PM
-                </li>
-              </ul>
-              <div className="d-flex justify-content-between">
-                <Button variant="primary" className="btn-block my-4">
-                  Join
-                </Button>
-                <Button variant="danger" className="btn-block my-4">
-                  Leave
-                </Button>
-                <Button
-                  variant="secondary"
-                  className="btn-block my-4"
-                  onClick={handleShowModal}
+      {convention ? (
+        <>
+          <Row>
+            <Col>
+              <Button
+                onClick={() => {
+                  handleClick();
+                }}
+                className="rounded-pill"
+                variant="light"
+              >
+                <FiEdit />
+              </Button>
+              <h1 className="display-4 text-center my-4">{convention?.name}</h1>
+              <div
+                style={{
+                  position: "relative",
+                  overflow: "hidden",
+                  borderRadius: "10px",
+                }}
+              >
+                <Image
+                  src={`http://localhost:8000/media/${convention?.convention_img}`}
+                  alt="Convention Image"
+                  fluid
+                  style={{ filter: "brightness(60%)" }}
+                  width="350px"
+                  height="200px"
+                />
+                <div
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    color: "#fff",
+                    zIndex: 1,
+                  }}
                 >
-                  Attendees
-                </Button>
+                  <h2 style={{ fontSize: "2rem" }}>{convention?.name}</h2>
+                </div>
               </div>
-            </div>
-          </div>
-        </Col>
-      </Row>
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Attendees</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form className="mb-3">
-            <Form.Group controlId="searchBar">
-              <Form.Control type="text" placeholder="Search for attendees" />
-            </Form.Group>
-          </Form>
-          <div>
-            {[...Array(20)].map((_, index) => (
-              <Card key={index} className="mb-3">
-                <Card.Body className="d-flex align-items-center">
-                  <Image
-                    src="https://scontent.ftlv19-1.fna.fbcdn.net/v/t1.6435-9/187676540_4191046497582597_4147289563785806834_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CYDM_dbnO98AX-JdaHe&_nc_ht=scontent.ftlv19-1.fna&oh=00_AfBzc9Zb80VKwqufIESNRJxCd5jdZcfKfQqe0G8w7A7FHQ&oe=646E41E4"
-                    width={60}
-                    height={60}
-                    roundedCircle
-                    className="me-3"
-                  />
-                  <div>
-                    <Card.Title className="mb-0">Noam Ezrot</Card.Title>
-                    <Card.Text className="text-muted">Attendee</Card.Text>
+              <div className="my-4">
+                <p className="lead">{convention?.description}</p>
+              </div>
+            </Col>
+            <Col lg={4} className="my-4">
+              <div className="card border-0 shadow-sm rounded">
+                <div className="card-body">
+                  <h3 className="card-title mb-3">Details</h3>
+                  <ul className="list-unstyled">
+                    <li>
+                      <strong>Address:</strong> {convention?.address}
+                    </li>
+                    <li>
+                      <strong>Capacity:</strong> {convention?.capacity}
+                    </li>
+                    <li>
+                      <strong>Start Time:</strong> {convention?.start_date}
+                    </li>
+                    <li>
+                      <strong>End Time:</strong> {convention?.end_date}
+                    </li>
+                  </ul>
+                  <div className="d-flex justify-content-between">
+                    <Button variant="primary" className="btn-block my-4">
+                      Join
+                    </Button>
+                    <Button variant="danger" className="btn-block my-4">
+                      Leave
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      className="btn-block my-4"
+                      onClick={handleShowModal}
+                    >
+                      Attendees
+                    </Button>
                   </div>
-                  <Button variant="outline-primary" className="ms-auto">
-                    Add Friend
-                  </Button>
-                </Card.Body>
-              </Card>
-            ))}
-          </div>
-        </Modal.Body>
-      </Modal>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>Attendees</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form className="mb-3">
+                <Form.Group controlId="searchBar">
+                  <Form.Control
+                    type="text"
+                    placeholder="Search for attendees"
+                  />
+                </Form.Group>
+              </Form>
+              <div>
+                {[...Array(20)].map((_, index) => (
+                  <Card key={index} className="mb-3">
+                    <Card.Body className="d-flex align-items-center">
+                      <Image
+                        src="https://scontent.ftlv19-1.fna.fbcdn.net/v/t1.6435-9/187676540_4191046497582597_4147289563785806834_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CYDM_dbnO98AX-JdaHe&_nc_ht=scontent.ftlv19-1.fna&oh=00_AfBzc9Zb80VKwqufIESNRJxCd5jdZcfKfQqe0G8w7A7FHQ&oe=646E41E4"
+                        width={60}
+                        height={60}
+                        roundedCircle
+                        className="me-3"
+                      />
+                      <div>
+                        <Card.Title className="mb-0">Noam Ezrot</Card.Title>
+                        <Card.Text className="text-muted">Attendee</Card.Text>
+                      </div>
+                      <Button variant="outline-primary" className="ms-auto">
+                        Add Friend
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                ))}
+              </div>
+            </Modal.Body>
+          </Modal>
+        </>
+      ) : (
+        ""
+      )}
     </Container>
   );
 }
--- code/server/backend/conventions/urls.py
+++ code/server/backend/conventions/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
 
     path('get-conventions/', views.getConventions),
+    path('get-convention/', views.getConvention),
 ]
