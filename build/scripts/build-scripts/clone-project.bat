4094cc87d2c897dacfd79a0fa39d308ded2d8c21
Aviram Dayan <aviram26@gmail.com>
2023-05-02 23:55:57 +0300

    https://gitlab.com/eden881/nomad/-/issues/39

--- build/scripts/build-scripts/clone-project.bat
+++ build/scripts/build-scripts/clone-project.bat
@@ -2,7 +2,7 @@
 
 echo.
 echo Cloning GIT repositories...
-git clone --quiet --single-branch --branch 39-build-aviram git@gitlab.com:eden881/nomad.git
+git clone --quiet --single-branch --branch main git@gitlab.com:eden881/nomad.git
 echo.
 echo Cleaning up development code from server...
 pushd %PROJECT_NAME%
