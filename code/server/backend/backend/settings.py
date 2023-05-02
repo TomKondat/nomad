696eb1324344fea42679d91fd0ecaa3c4e4a0f58
Aviram Dayan <aviram26@gmail.com>
2023-05-02 23:32:49 +0300

    https://gitlab.com/eden881/nomad/-/issues/39

--- code/server/backend/backend/settings.py
+++ code/server/backend/backend/settings.py
@@ -9,13 +9,17 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
-from pathlib import Path
 import os
+from os.path import dirname, abspath
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+ENV_FILE_DIR = dirname(dirname(abspath(__file__)))
+load_dotenv(os.path.join(ENV_FILE_DIR, '.env'))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -26,8 +30,7 @@ SECRET_KEY = 'django-insecure-dk9-3=i)6wmp8^nv50o1(oe$alttrk^e@eg)j2f!5ma_9$z20h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 CORS_ORIGIN_ALLOW_ALL = True
@@ -128,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -145,3 +149,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ]
 }
+
+API_PREFIX = os.environ.get('API_PREFIX', default='')
--- code/server/backend/backend/wsgi.py
+++ code/server/backend/backend/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+SERVER_BASE = 'c:/Apache24/_projects/nomad/server'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+sys.path.append(SERVER_BASE)
+sys.path.append(f'{SERVER_BASE}/backend')
+sys.path.append(f'{SERVER_BASE}/chat_messages')
+sys.path.append(f'{SERVER_BASE}/conventions')
+sys.path.append(f'{SERVER_BASE}/organizations')
+sys.path.append(f'{SERVER_BASE}/profiles')
+
 
 application = get_wsgi_application()
