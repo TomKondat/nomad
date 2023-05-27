fcf2309dff7f45d61810ab428bd1133c678202f9
Tom Kondat <tomkondat5@gmail.com>
2023-05-27 18:31:34 +0300

    remove import and fix function return

--- code/client/src/Map.js
+++ code/client/src/Map.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 import "./styles.css";
+import Geocode from "react-geocode";
 
-function Map() {
+function Map(props) {
+  const { address } = props;
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyCSG8dtnlKzB8JprbHIMtIUUZAL6F4X2Yc",
   });
+  const [coordinates, setCoordinates] = useState({
+    lat: 31.5090643,
+    lng: 34.597014,
+  });
+
+  useEffect(() => {
+    LoadGeocoding(address);
+  }, []);
+
   if (!isLoaded) return <div>Loading...</div>;
 
-  const center = { lat: 31.5090643, lng: 34.597014 };
+  async function LoadGeocoding(address) {
+    Geocode.setApiKey("AIzaSyCSG8dtnlKzB8JprbHIMtIUUZAL6F4X2Yc");
+    Geocode.setLanguage("en");
+    Geocode.setRegion("il");
+
+    const response = await Geocode.fromAddress(address);
+    const { lat, lng } = response.results[0].geometry.location;
+    setCoordinates({ lat: lat, lng: lng });
+  }
+
   return (
-    <GoogleMap zoom={13} center={center} mapContainerClassName="map-container">
-      <MarkerF position={center} />
+    <GoogleMap
+      zoom={13}
+      center={coordinates}
+      mapContainerClassName="map-container"
+    >
+      <MarkerF position={coordinates} />
     </GoogleMap>
   );
 }
