975961271c4cf78c1985758495367e142f53f3bd
Tom Kondat <tomkondat5@gmail.com>
2023-06-02 01:09:54 +0300

    Build the register convention query and serializer

--- code/server/backend/conventions/views.py
+++ code/server/backend/conventions/views.py
@@ -2,8 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import Conventions, Organization
-from .serializers import ConventionSerializer
+from .models import Conventions, Organization, Registration
+from .serializers import ConventionSerializer,RegistrationSerializer
 
 
 @api_view(['POST'])
@@ -34,3 +34,14 @@ def getConvention(request):
             id=convention['organization_id_id'])[0]
         convention['organization'] = organization
     return Response(convention)
+
+# send a user and convention id to register a user for a convention
+@api_view(['POST'])
+def register(request):
+    serializer = RegistrationSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'message': 'Registration created successfully'}, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
